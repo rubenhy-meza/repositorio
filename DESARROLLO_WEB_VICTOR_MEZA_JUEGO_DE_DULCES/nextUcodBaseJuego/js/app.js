b3b2f2ca187b85f6608dadc540e3a1409be93000
rubenhy <rubenhy_90@hotmail.com>
2019-06-01 20:45:15 -0500

    prueba final
    jueo de dulces etapa prueba

--- DESARROLLO_WEB_VICTOR_MEZA_JUEGO_DE_DULCES/nextUcodBaseJuego/js/app.js
+++ DESARROLLO_WEB_VICTOR_MEZA_JUEGO_DE_DULCES/nextUcodBaseJuego/js/app.js
@@ -28,18 +28,13 @@ $(document).ready(function() {
 
 }
 });
-
+      
 $(".btn-reinicio").click(function(){
-
-          if ($('.btn-reinicio').text() == "Iniciar") {
-            var tiempo = 60 * 2; //Segundos para el juego
+	   if ($('.btn-reinicio').text() == "Iniciar") {
+            var tiempo = 60 * 0.25; //Segundos para el juego
             startTimer(tiempo, $("#timer"));
             $('.btn-reinicio').text('Reiniciar');
-        } else {
-            //location.reload();
-        }
-
-	i=0;
+            i=0;
 	score=0;
 	mov=0;
 	$(".panel-score").css("width","25%");
@@ -47,7 +42,6 @@ $(".btn-reinicio").click(function(){
 	$(".time").show();
 	$("#score-text").html("0");
 	$("#movimientos-text").html("0");
-	$(this).html("Reiniciar")
 	clearInterval(intervalo);
 	clearInterval(eliminar);
 	clearInterval(nuevosDulces);
@@ -58,10 +52,19 @@ $(".btn-reinicio").click(function(){
 	intervalo=setInterval(function(){
 		desplazamiento()
 	},500);
-	tiempo=setInterval(function(){
-		timer()
-	},1000);
+	 
+
+        } else {
+            location.reload();
+        }  
+   
+
+   
+   
+     
+	
 });
+
   
 // Iniciar timer
 function startTimer(duration, display) {
@@ -75,9 +78,16 @@ function startTimer(duration, display) {
 
         $(display).text(minutes + ":" + seconds);
 
-        if (--timer < 0) {//terminarJuego();
+        if (--timer < 0) {
+        	clearInterval(eliminar);
+			clearInterval(nuevosDulces);
+			clearInterval(intervalo);
+			clearInterval(tiempo);
+			$(".panel-tablero").hide("drop","slow",$( ".panel-score" ).animate({width:'100%'},3000));
+			$(".time").hide();
             $("body").trigger("finTiempo");
 			clearInterval(interval)
+
         }
     }, 1000);
 }
@@ -104,13 +114,13 @@ function desplazamiento(){
 	clearInterval(intervalo);
 	eliminar=setInterval(function(){
 		eliminarDulces()
-	},100);}
+	},200);}
 };
 //eliminar dulces del panel
 function eliminarDulces(){
 	matriz=0;
-	busquedaHorizontal=horizontal();
-	busquedaVertical=vertical();
+	busquedaHorizontal=buscarHorizontal();
+	busquedaVertical=buscarVertical();
 	for(var j=1;j<8;j++){
 		matriz=matriz+$(".col-"+j).children().length;}
 
@@ -121,18 +131,20 @@ function eliminarDulces(){
 		buscarNuevosDulces=0;
 		nuevosDulces=setInterval(function(){
 			nuevosdulces()
-		},600);}
+		},500);}
 
 	if(busquedaHorizontal==1||busquedaVertical==1){
 		$(".elemento").draggable({disabled:true});
 		$("div[class^='col']").css("justify-content","flex-end");
-		$(".activo").hide("pulsate",1000,function(){
+		$(".activo").hide("pulsate",500,function(){
 			var scoretmp=$(".activo").length;
 			$(".activo").remove("img");
 			score=score+scoretmp*10;
 			$("#score-text").html(score);//Cambiamos la puntuación
 		});
 	}
+	// si no hay dulces iguales
+
 	if(busquedaHorizontal==0 && busquedaVertical==0 && matriz==49){
 		$(".elemento").draggable({
 			disabled:false,
@@ -155,8 +167,8 @@ function eliminarDulces(){
 			do{
 				espera=dropped.swap($(droppedOn));}
 			while(espera==0);
-			busquedaHorizontal=horizontal();
-			busquedaVertical=vertical();
+			busquedaHorizontal=buscarHorizontal();
+			busquedaVertical=buscarVertical();
 			if(busquedaHorizontal==0 && busquedaVertical==0){
 				dropped.swap($(droppedOn));}
 			if(busquedaHorizontal==1 || busquedaVertical==1){
@@ -164,6 +176,93 @@ function eliminarDulces(){
 				clearInterval(eliminar);
 				eliminar=setInterval(function(){
 					eliminarDulces()
-				},150);}},
+				},10);}},
 	});
+};
+//buscar dulce horizontalmente
+function buscarHorizontal(){
+	var busquedaH=0;
+	for(var j=1;j<8;j++){
+		for(var k=1;k<6;k++){
+			var res1=$(".col-"+k).children("img:nth-last-child("+j+")").attr("src");
+			var res2=$(".col-"+(k+1)).children("img:nth-last-child("+j+")").attr("src");
+			var res3=$(".col-"+(k+2)).children("img:nth-last-child("+j+")").attr("src");
+			if((res1==res2) && (res2==res3) && (res1!=null) && (res2!=null) && (res3!=null)){
+				$(".col-"+k).children("img:nth-last-child("+(j)+")").attr("class","elemento activo");
+				$(".col-"+(k+1)).children("img:nth-last-child("+(j)+")").attr("class","elemento activo");
+				$(".col-"+(k+2)).children("img:nth-last-child("+(j)+")").attr("class","elemento activo");
+				busquedaH=1;
+			}
+		}
+	}
+	return busquedaH;
+};
+// buscar dulce en vertical
+
+function buscarVertical(){
+	var busquedaV=0;
+	for(var l=1;l<6;l++){
+		for(var k=1;k<8;k++){
+			var res1=$(".col-"+k).children("img:nth-child("+l+")").attr("src");
+			var res2=$(".col-"+k).children("img:nth-child("+(l+1)+")").attr("src");
+			var res3=$(".col-"+k).children("img:nth-child("+(l+2)+")").attr("src");
+			if((res1==res2) && (res2==res3) && (res1!=null) && (res2!=null) && (res3!=null)){
+				$(".col-"+k).children("img:nth-child("+(l)+")").attr("class","elemento activo");
+				$(".col-"+k).children("img:nth-child("+(l+1)+")").attr("class","elemento activo");
+				$(".col-"+k).children("img:nth-child("+(l+2)+")").attr("class","elemento activo");
+				busquedaV=1;
+			}
+		}
+	}
+	return busquedaV;
+};
+// crear dulces nuevos
+
+function nuevosdulces(){
+	$(".elemento").draggable({disabled:true});
+	$("div[class^='col']").css("justify-content","flex-start")
+	for(var j=1;j<8;j++){
+		lencolum[j-1]=$(".col-"+j).children().length;}
+	if(buscarNuevosDulces==0){
+		for(var j=0;j<7;j++){
+			lenrest[j]=(7-lencolum[j]);}
+		maximo=Math.max.apply(null,lenrest);
+		contadorTotal=maximo;}
+	if(maximo!=0){
+		if(buscarNuevosDulces==1){
+			for(var j=1;j<8;j++){
+				if(contadorTotal>(maximo-lenrest[j-1])){
+					$(".col-"+j).children("img:nth-child("+(lenrest[j-1])+")").remove("img");}}
+		}
+		if(buscarNuevosDulces==0){
+			buscarNuevosDulces=1;
+			for(var k=1;k<8;k++){
+				for(var j=0;j<(lenrest[k-1]-1);j++){
+					$(".col-"+k).prepend("<img src='' class='elemento' style='visibility:hidden'/>");}}
+		}
+		for(var j=1;j<8;j++){
+			if(contadorTotal>(maximo-lenrest[j-1])){
+				numero=Math.floor(Math.random()*4)+1;
+				imagen="image/"+numero+".png";
+				$(".col-"+j).prepend("<img src="+imagen+" class='elemento'/>");}
+		}
+	}
+	if(contadorTotal==1){
+		clearInterval(nuevosDulces);
+		eliminar=setInterval(function(){
+			eliminarDulces()
+		},150);
+	}
+	contadorTotal=contadorTotal-1;
+};
+// intercambiar dulces
+
+jQuery.fn.swap=function(b){
+	b=jQuery(b)[0];
+	var a=this[0];
+	var t=a.parentNode.insertBefore(document.createTextNode(''),a);
+	b.parentNode.insertBefore(a,b);
+	t.parentNode.insertBefore(b,t);
+	t.parentNode.removeChild(t);
+	return this;
 };

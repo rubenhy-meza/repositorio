f702cf0dac123c9e53257b11916d79b663b46266
rubenhy <rubenhy_90@hotmail.com>
2019-08-05 19:10:16 -0500

    app.js
    avanze

--- Buscador-node/public/app.js
+++ Buscador-node/public/app.js
@@ -13,7 +13,62 @@ $(document).ready(function() {
 
     init();
     setSearch();
-    
+
+     // makes ajax request to get all or filered data
+    $('#buscar').click(function() {
+        if ($("#checkPersonalizada")[0].checked){
+            var valores = $("#rangoPrecio").val();
+            valores = valores.split(";");
+            var urls = `http://localhost:3000/ciudad/${$("#ciudad").val()}/tipo/${$("#tipo").val()}/desde/${valores[0]}/hasta/${valores[1]}`;
+        } else {
+            var urls = "http://localhost:3000/search";
+        }
+        $.ajax({
+            url: urls,
+            type: 'get',
+            dataType: 'json'
+          })
+            .done(function(data){
+                if (!data.error) {
+                    // console.log(data);
+                    $('.lista').html(renderCard(data.datos));
+                }
+            });
+    });
+     function renderSelect(data) { 
+        // data must be an array ['NY', 'Miami', 'LA']
+        var html = '';
+        data.forEach(function(key, idx) {
+            html += `<option value="${key}">${key}</option>`;
+        });
+        return html;
+    }
+
+    // genereate HTML/Template for a 'card' / 'house available'
+    function renderCard(objArr) {
+        var html = '';
+        
+        objArr.forEach(function(key, idx)
+        {
+            html += `<div class="card horizontal">
+                    <div class="card-image">
+                        <img src="http://localhost:3000/img/home.jpg">
+                    </div>
+                    <div class="card-stacked">
+                        <div class="card-content">
+                            <div> <p><strong>Direccion: </strong>${ key.Direccion }</p> </div>
+                            <div> <p><strong>Ciudad: </strong>${ key.Ciudad }</p> </div>
+                            <div> <p><strong>Telefono: </strong>${ key.Telefono }</p> </div>
+                            <div> <p><strong>Código postal: </strong>${ key.Codigo_Postal }</p> </div>
+                            <div> <p><strong>Precio: </strong>${ key.Precio }</p> </div>
+                            <div> <p><strong>Tipo: </strong>${ key.Tipo }</p> </div>
+                        </div>
+                    </div>
+                </div>`;
+        });
+        return html;
+    } // .renderCard
+
  function init(){
         $.ajax({
             url: 'http://localhost:3000/filteroptions',
